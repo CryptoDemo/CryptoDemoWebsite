461dcd272fa851e1ddcaab8b797157117c6df954
Oscar-roqsoft <102908874+Oscar-roqsoft@users.noreply.github.com>
2024-04-17 16:06:02 +0100

    update

--- src/components/header.vue
+++ src/components/header.vue
@@ -10,9 +10,9 @@
 
         <div style="position: relative"> <NuxtLink to="#"> <img :src="props.Menuicon" /> </NuxtLink> </div>
         
-        <div style="display: flex;">
+        <div style="display: flex;margin-inline-start: auto;">
           <span class="flex-lg-and-up hidden-sm-and-down mt-4" :class="isDark ? 'text1':'text1-light'" style="align-items: center; display: flex;">{{props.text2}}</span>
-          <NuxtLink :to="props.link" class="mt-4 text2"> <span :class="isDark ? 'text2':'text2-light'">{{props.title}}</span></NuxtLink>
+          <NuxtLink :to="props.link" class="mt-4 d-flex text2"> <span :class="isDark ? 'text2':'text2-light'">{{props.title}}</span></NuxtLink>
  
 
           <div v-if="hide"   style="width: 400px; height: 61px ; flex-shrink: 0; border-radius: 20px; background: #161D26; display: flex;position: absolute; right: 54%">
