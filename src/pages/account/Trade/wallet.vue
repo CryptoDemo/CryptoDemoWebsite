25ee1fb06312f4b5a243a653acee427eb51d5906
Oscar-roqsoft <102908874+Oscar-roqsoft@users.noreply.github.com>
2024-06-04 08:57:12 +0100

    update

--- src/pages/account/Trade/wallet.vue
+++ src/pages/account/Trade/wallet.vue
@@ -124,7 +124,7 @@
                                     </td>
                                 </div>
                               
-                              <div style="display: flex; justify-content: end;">
+                              <div class="mt-3" style="display: flex; justify-content: end;">
                                 <v-btn class="primary-btn1" style="border-radius: 10px !important; color: white; padding: 20px; width: 30%;"
                                   text="Cancel"
                                   @click="isActive.value = false"
