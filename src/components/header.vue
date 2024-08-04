28e474b38e5879d4b95cf50a6ceff9cc37992a94
MAC <mac@MACs-MacBook-Pro.local>
2024-08-04 05:40:54 +0100

    update

--- src/components/header.vue
+++ src/components/header.vue
@@ -48,13 +48,13 @@
         
           <div v-if="wallet" style="display: grid; margin-top: 25px; margin-inline-end: -4px;">
             <nuxt-link to="/account/trade/wallet">
-              <v-btn class="nav-btn wallet-btn" :class="isDark ? 'nav-btn':'nav-btn-light'"> 
+              <button class="nav-btn wallet-btn" :class="isDark ? 'nav-btn':'nav-btn-light'"> 
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" :class="isDark ? 'close-btn':'close-btn-dark'">
                   <path d="M22.7805 12.6201V14.6801C22.7805 15.2401 22.3205 15.7001 21.7505 15.7001H19.8205C18.7405 15.7001 17.7505 14.9101 17.6605 13.8301C17.6005 13.2001 17.8405 12.6101 18.2605 12.2001C18.6305 11.8201 19.1405 11.6001 19.7005 11.6001H21.7505C22.3205 11.6001 22.7805 12.0601 22.7805 12.6201Z" />
                   <path d="M16.1603 13.9599C16.0703 12.9099 16.4503 11.8799 17.2103 11.1299C17.8503 10.4799 18.7403 10.0999 19.7003 10.0999H20.2703C20.5503 10.0999 20.7803 9.8699 20.7403 9.5899C20.4703 7.6499 18.7903 6.1499 16.7803 6.1499H6.78027C4.57027 6.1499 2.78027 7.9399 2.78027 10.1499V17.1499C2.78027 19.3599 4.57027 21.1499 6.78027 21.1499H16.7803C18.8003 21.1499 20.4703 19.6499 20.7403 17.7099C20.7803 17.4299 20.5503 17.1999 20.2703 17.1999H19.8203C17.9203 17.1999 16.3203 15.7799 16.1603 13.9599ZM13.7803 11.8999H7.78027C7.37027 11.8999 7.03027 11.5699 7.03027 11.1499C7.03027 10.7299 7.37027 10.3999 7.78027 10.3999H13.7803C14.1903 10.3999 14.5303 10.7399 14.5303 11.1499C14.5303 11.5599 14.1903 11.8999 13.7803 11.8999Z"/>
                   <path d="M14.9899 3.9801C15.2499 4.2501 15.0199 4.6501 14.6399 4.6501H6.80994C5.71994 4.6501 4.69994 4.9701 3.84994 5.5201C3.45994 5.7701 2.92994 5.5001 3.11994 5.0701C3.67994 3.7601 4.98994 2.8501 6.49994 2.8501H12.1199C13.2799 2.8501 14.3099 3.2601 14.9899 3.9801Z"/>
                 </svg>
-              </v-btn>
+              </button>
             </nuxt-link>
             <span  class="nav-icon-text mb-3 flex-lg-and-up hidden-sm-and-down"  :class="isDark ? 'text-dark':'text-light'" style="margin-left: 3px;">Wallet</span>     
           </div>
@@ -320,10 +320,13 @@ margin-top: 15px;
 .nav-btn {
 border-radius: 20px; 
 background: #10192D !important; 
-max-width: 52px!important;
+width: 52px!important;
 height: 53.2px !important;
 flex-shrink: 0;
 box-shadow: none;
+display: flex;
+align-items: center;
+justify-content: center;
 }
 .nav-btn-dark{
 background: #10192D !important; 
@@ -424,9 +427,9 @@ margin-top: 15px !important;
   right: 54px !important;
   margin-top: 27px !important;
 }
-/* .wallet-btn{
+.wallet-btn{
 margin-bottom: 24px;
-} */
+}
 }
 
 
